129ada178ceb810049af0344cb7fada129152138
SeungyoupBaek <bsy8220@naver.com>
2014-01-16 13:22:54 +0900

    실습 완료했습니다.

--- CameraLibrary/ViewController.m
+++ CameraLibrary/ViewController.m
@@ -9,10 +9,47 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
 @end
 
 @implementation ViewController
+- (IBAction)takePicture:(id)sender {
+    if(![UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]){
+        //에러 처리
+        UIAlertView* alert = [[UIAlertView alloc] initWithTitle: @"오류"message:@"카메라가 지원되지 않는 기종입니다" delegate:self cancelButtonTitle:@"확인" otherButtonTitles: nil];
+        [alert show];
+        return;
+    }
+    UIImagePickerController *imagePicker = [[UIImagePickerController alloc] init];
+    imagePicker.delegate = self;
+    imagePicker.allowsEditing = YES;
+    imagePicker.sourceType = UIImagePickerControllerSourceTypeCamera;
+    [self presentViewController:imagePicker animated:YES completion:Nil];
+}
+
+- (IBAction)getImage:(id)sender {
+    UIImagePickerController *imagePicker = [[UIImagePickerController alloc] init];
+    imagePicker.delegate = self;
+    imagePicker.allowsEditing = YES;
+    imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    [self presentViewController:imagePicker animated:YES completion:nil];
+}
+
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info{
+    UIImage *editedImage = [info objectForKey:UIImagePickerControllerEditedImage];
+    UIImage *originalImage = [info objectForKey:UIImagePickerControllerEditedImage];
+    
+    // 편집된 이미지가 있으면 사용, 없으면 원본으로 사용
+    UIImage *usingImage = (nil == editedImage) ? originalImage : editedImage;
+    self.imageView.image = usingImage;
+    
+    // 피커 감추기
+    [picker dismissViewControllerAnimated:YES completion:nil];
+}
+
+
+
 
 - (void)viewDidLoad
 {
